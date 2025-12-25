a9852cb4764e9f2bdabdb5111eea6a020191d4ef
Sankalpa061 <sankalpa061@gmail.com>
2025-12-25 19:10:48 +0545

    utils added

---
+++ src/utils/ApiError.js
@@ -0,0 +1,12 @@
+class apiError extends Error{
+    constructor(statusCode, message="Something went wrong!!", error=[], stack = ""){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null;
+        this.message = message
+        this.success = false;
+        this.error = error
+    }
+}
+
+export {apiError}
