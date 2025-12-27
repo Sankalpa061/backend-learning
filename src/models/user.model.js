f6a5178efa6a4863e7c41128be4c8c96cf978327
Sankalpa061 <sankalpa061@gmail.com>
2025-12-27 09:04:53 +0545

    changes vs code theme

---
+++ src/models/user.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    }
+});
+
+export const User = mongoose.model("User",userSchema)
