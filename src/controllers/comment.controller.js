961b9f65c039f975584a08e2925d6bff89c79e18
Sankalpa061 <sankalpa061@gmail.com>
2026-05-17 13:38:58 +0545

    add getVideoComments and addcomment controllers

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,11 +8,57 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+    const skip = ( pageNumber - 1 ) * limitNumber;
+
+    const filter = {};
+    if(!videoId){
+        throw new ApiError(400,"Invalid video ID!!");
+    }
+    filter.video = videoId; 
+    const totalComments = await Comment.countDocuments(filter);
+    const comments = await Comment.find(filter)
+    .skip(skip)
+    .limit(limitNumber);
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{
+            comments,
+            totalComments,
+            totalPage : Math.ceil(totalComments/limitNumber)
+        },"All the comments for this video are successfully displayed!!")
+    )
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params;
+    const {content} = req.body;
+    if (!content) {
+        throw new ApiError(400,"Empty content cannot be published!!")
+    }
+    if (!videoId) {
+        throw new ApiError(400,"Must provide a videoId!!")
+    }
+
+    const comment = await Comment.create({
+            content,
+            video: videoId,
+            owner: req.user._id,
+        }) 
+    
+    if(!comment){
+        throw new ApiError(500,"Comment object could not be created!!");
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,comment, "Comment Successfully posted!!")
+    );
 })
 
 const updateComment = asyncHandler(async (req, res) => {
