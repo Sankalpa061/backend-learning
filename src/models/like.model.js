d6e3c3af22d3efe87a342923b3961cbfe97190f9
Sankalpa061 <sankalpa061@gmail.com>
2026-05-22 16:30:25 +0545

    added like controller, like model and like route

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,4 +1,5 @@
 import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const likeSchema = new Schema(
     {
         video: {
@@ -23,5 +24,5 @@ const likeSchema = new Schema(
     {timestamps: true}
 )
 
-commentSchema.plugin(mongooseAggregatePaginate)
+likeSchema.plugin(mongooseAggregatePaginate)
 export const Like = mongoose.model("Like",likeSchema)
