16a595fd15182bf8a07c9d257f1550ad41f31b73
Sankalpa061 <sankalpa061@gmail.com>
2026-05-22 15:54:02 +0545

    add updatedComment and deleteComment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -63,10 +63,53 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params;
+    const {content} = req.body;
+    if (!content) {
+        throw new ApiError(400,"The content of the new comment cannot be empty!!");
+    }
+    const updatedComment = await Comment.findByIdAndUpdate(commentId,{
+        $set:{
+            content
+        }
+    },{ new: true })
+
+    if (!updatedComment) {
+        throw new ApiError(500,"Comment details didnot update!!");
+    }
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(200, updateComment, "Comment Updated successfully!!")
+    );
+
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params;
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+        throw new ApiError(404,"Comment with the provided ID doesnot exist!!");
+    }
+
+    if (!comment.owner.equals(req.user._id)) {
+        throw new ApiError(401,"User is not Authorized to perform the delete operation!!")
+    }
+
+    const response = await Comment.findByIdAndDelete(commentId);
+    if (!response) {
+        throw new ApiError(500, "Unable to delete the comment!!");
+    }
+
+    res
+    .status(200)
+    .json(
+        new ApiResponse(200, response, "Comment Deleted Successfully!!")
+    );
+
+
 })
 
 export {
