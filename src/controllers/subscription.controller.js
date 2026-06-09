6ee9d13ed4091e71a499b393ac842d44f4d67f84
Sankalpa061 <sankalpa061@gmail.com>
2026-06-09 12:47:03 +0545

    fix: correct subscription router import and endpoint registration; enhance toggleSubscription logic with error handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -4,11 +4,41 @@ import { Subscription } from "../models/subscription.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { json, response } from "express"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if (!channelId) {
+        throw new ApiError(400,"channel Id is not provided!!")
+    }
+
+    const filter = {};
+    filter.subscriber = req.user._id;
+    filter.channel = channelId;
+
+    const subscribed = await Subscription.findOne(filter);
+    if(subscribed){
+        const response = await subscribed.deleteOne();
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,"Channel Unsubscribed Successfully!!")
+        )
+    }
+    const response = await Subscription.create({
+        channel : channelId,
+        subscriber : req.user._id
+    })
+    if (!response) {
+        throw new ApiError(500,"Error while subscribing to the channel!!")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,response,"Channel Subscribed Successfully!!")
+    )
 })
 
 // controller to return subscriber list of a channel
